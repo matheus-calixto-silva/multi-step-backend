8ab755b7a43cbcd9e090396c8b7024569dc3dfc6
matheus-calixto-silva <matheuscalixto8@gmail.com>
2026-04-02 03:33:37 -0300

    feat(app): configura AppModule com ThrottlerModule, ScheduleModule e ThrottlerGuard global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { RegistrationModule } from './registration/registration.module';
+import { CepModule } from './providers/cep/cep.module';
+import { APP_GUARD } from '@nestjs/core';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -21,6 +27,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         synchronize: true, // ⚠️ Trocar para false + migration antes da entrega
       }),
     }),
+    ThrottlerModule.forRoot([{ ttl: 60_000, limit: 30 }]),
+    RegistrationModule,
+    CepModule,
   ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
