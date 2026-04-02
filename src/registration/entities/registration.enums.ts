4274a6b4234b42496efc6a408418c0ff90b48026
matheus-calixto-silva <matheuscalixto8@gmail.com>
2026-04-01 23:56:20 -0300

    feat(registration): adiciona enums RegistrationStep e DocumentType

---
+++ src/registration/entities/registration.enums.ts
@@ -0,0 +1,13 @@
+export enum RegistrationStep {
+  IDENTIFICATION = 'IDENTIFICATION',
+  DOCUMENT = 'DOCUMENT',
+  CONTACT = 'CONTACT',
+  ADDRESS = 'ADDRESS',
+  REVIEW = 'REVIEW',
+  COMPLETED = 'COMPLETED',
+}
+
+export enum DocumentType {
+  CPF = 'CPF',
+  CNPJ = 'CNPJ',
+}
